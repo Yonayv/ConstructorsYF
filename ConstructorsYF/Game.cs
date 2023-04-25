db289baed9e82d6d65d3010570698b3654d93678
Yonayv <fixicoyonayv@gmail.com>
2023-04-25 13:20:34 -0500

    WOoP

--- ConstructorsYF/Game.cs
+++ ConstructorsYF/Game.cs
@@ -8,6 +8,13 @@ namespace ConstructorsYF
 {
     class Game
     {
+        /// <summary>
+        /// The Game class constructer was made to initialize the params values
+        /// The public string game holds the the strings' titleParam and genreParam
+        /// The title and genres' value equal to the titleParam and genreParam
+        /// 
+        /// </summary>
+        /// 
 
         public string title, genre; 
 
--- ConstructorsYF/Program.cs
+++ ConstructorsYF/Program.cs
@@ -4,12 +4,23 @@ namespace ConstructorsYF
 {
     class Program
     {
+        /// <summary>
+        /// The Program class holds the main method and initializes the values of a favorite game
+        /// the title and genre of the Game object is printed to the console
+        /// another Game object is made without any initialized values
+        /// the title and genre of the other Game object to the console
+        /// </summary>
+        /// <param name="args"></param>
+        /// 
         static void Main(string[] args)
         {
-            Game myGame = new Game("CS:GO", "M for Mature");
+            Game myGame = new Game("CS:GO", "FPS");
 
-            Console.WriteLine($"My favorite game is {myGame.title} and it is rated {myGame.genre}!");
+            Console.WriteLine($"My favorite game is {myGame.title} and it is a {myGame.genre} game!");
 
+            Game myOtherGame = new Game();
+
+            Console.WriteLine($"My favorite game is {myOtherGame.title} and it is rated {myOtherGame.genre}!");
         }
     }
 }
